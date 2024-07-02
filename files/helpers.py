3356d711ee016b8f8cdb2b8446a3a656d6f6fdfa
the80hz <dnl.vlgv@mail.ru>
2024-07-03 02:35:46 +0400

    exif metadata not deleting, only gpt and date update +bugged alphatest

--- files/helpers.py
+++ files/helpers.py
@@ -126,37 +126,30 @@ def change_to_rational(number):
 def set_exif(filepath: str, lat, lng, altitude, timestamp):
     exif_dict = piexif.load(filepath)
 
-    date_time = datetime.fromtimestamp(timestamp).strftime("%Y:%m:%d %H:%M:%S")  # Create date object
+    # Update date and time information
+    date_time = datetime.fromtimestamp(timestamp).strftime("%Y:%m:%d %H:%M:%S")
     exif_dict['0th'][piexif.ImageIFD.DateTime] = date_time
     exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = date_time
     exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = date_time
 
+    # GPS information
+    lat_deg = to_deg(lat, ["S", "N"])
+    lng_deg = to_deg(lng, ["W", "E"])
+
+    exiv_lat = (change_to_rational(lat_deg[0]), change_to_rational(lat_deg[1]), change_to_rational(lat_deg[2]))
+    exiv_lng = (change_to_rational(lng_deg[0]), change_to_rational(lng_deg[1]), change_to_rational(lng_deg[2]))
+
+    gps_ifd = {
+        piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
+        piexif.GPSIFD.GPSLatitudeRef: lat_deg[3],
+        piexif.GPSIFD.GPSLatitude: exiv_lat,
+        piexif.GPSIFD.GPSLongitudeRef: lng_deg[3],
+        piexif.GPSIFD.GPSLongitude: exiv_lng,
+        piexif.GPSIFD.GPSAltitudeRef: 1,
+        piexif.GPSIFD.GPSAltitude: change_to_rational(round(altitude, 2))
+    }
+
+    exif_dict['GPS'] = gps_ifd
+
     exif_bytes = piexif.dump(exif_dict)
     piexif.insert(exif_bytes, filepath)
-
-    try:
-        exif_dict = piexif.load(filepath)
-        lat_deg = to_deg(lat, ["S", "N"])
-        lng_deg = to_deg(lng, ["W", "E"])
-
-        exiv_lat = (change_to_rational(lat_deg[0]), change_to_rational(lat_deg[1]), change_to_rational(lat_deg[2]))
-        exiv_lng = (change_to_rational(lng_deg[0]), change_to_rational(lng_deg[1]), change_to_rational(lng_deg[2]))
-
-        gps_ifd = {
-            piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
-            piexif.GPSIFD.GPSAltitudeRef: 1,
-            piexif.GPSIFD.GPSAltitude: change_to_rational(round(altitude, 2)),
-            piexif.GPSIFD.GPSLatitudeRef: lat_deg[3],
-            piexif.GPSIFD.GPSLatitude: exiv_lat,
-            piexif.GPSIFD.GPSLongitudeRef: lng_deg[3],
-            piexif.GPSIFD.GPSLongitude: exiv_lng,
-        }
-
-        exif_dict['GPS'] = gps_ifd
-
-        exif_bytes = piexif.dump(exif_dict)
-        piexif.insert(exif_bytes, filepath)
-
-    except Exception as e:
-        logging.error(f"Error setting GPS coordinates for {filepath}: {e}")
-        pass
--- files/main.py
+++ files/main.py
@@ -1,21 +1,18 @@
 from helpers import search_media, create_folders, set_exif, set_file_times, copy_folder, delete_dir, copy_files_only
 import json
-from PIL import Image, ImageOps
+from PIL import Image
 import os
 from typing import List
 from os import DirEntry
 from pathlib import Path
-import logging
 
-# Setup logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def merge_folder(browser_path: str, window, edited_word):
     piexif_codecs = [k.casefold() for k in ['TIF', 'TIFF', 'JPEG', 'JPG']]
+    video_codecs = [k.casefold() for k in ['MP4', 'MOV']]
 
-    media_moved = []  # array with names of all the media already matched
-    original_folder = Path(browser_path)  # source path
-    # create output directories for merged and unmerged files
+    media_moved = []
+    original_folder = Path(browser_path)
     output_folder = original_folder.parent / (original_folder.name + " - merged")
     matched_output_folder = output_folder / "matched"
     edited_output_folder = output_folder / "edited_raw"
@@ -26,104 +23,84 @@ def merge_folder(browser_path: str, window, edited_word):
     edited_word = edited_word or "edited"
 
     try:
-        # clear output folder
         delete_dir(output_folder)
         create_folders(output_folder, matched_output_folder, unmatched_output_folder, edited_output_folder)
-        # copy all files to the output folder to leave the original intact
         copy_folder(original_folder, output_folder)
 
-        files_in_dir: List[DirEntry] = list(os.scandir(output_folder))  # Convert iterator into a list to sort it
-        files_in_dir.sort(key=lambda s: len(s.name))  # Sort by length to avoid name(1).jpg be processed before name.jpg
+        files_in_dir: List[DirEntry] = list(os.scandir(output_folder))
+        files_in_dir.sort(key=lambda s: len(s.name))
     except FileNotFoundError:
         window['-PROGRESS_LABEL-'].update("Choose a valid directory", visible=True, text_color='red')
-        logging.error("Invalid directory selected.")
         return
 
-    # Get JSON files
     json_files = list(filter(lambda x: x.is_file() and x.name.endswith(".json"), files_in_dir))
     for entry in json_files:
         if entry.name == "metadata.json":
-            # skip album metadata files
             continue
 
-        with open(entry, encoding="utf8") as f:  # Load JSON into a var
+        with open(entry, encoding="utf8") as f:
             data = json.load(f)
 
         progress = round(json_files.index(entry) / len(json_files) * 100, 2)
         window['-PROGRESS_LABEL-'].update(str(progress) + "%", visible=True)
         window['-PROGRESS_BAR-'].update(progress, visible=True)
 
-        # SEARCH MEDIA ASSOCIATED TO JSON
-        original_title = data['title']  # Store metadata into vars
+        original_title = data['title']
 
         try:
             title = search_media(output_folder, original_title, media_moved, edited_output_folder, edited_word)
-
         except Exception as e:
-            logging.error(f"Error on search_media() with file {original_title}: {e}")
+            print(f"Error on search_media() with file {original_title}: {e}")
             error_counter += 1
             continue
 
         filepath = output_folder / title
         if title == "None":
-            logging.warning(f"{original_title} not found")
+            print(original_title + " not found")
             error_counter += 1
             continue
 
-        # METADATA EDITION
-        time_stamp = int(data['photoTakenTime']['timestamp'])  # Get creation time
-        logging.info(f"Processing file: {filepath}")
+        time_stamp = int(data['photoTakenTime']['timestamp'])
+        print(filepath)
 
-        if title.rsplit('.', 1)[1].casefold() in piexif_codecs:  # If EXIF is supported
+        file_extension = title.rsplit('.', 1)[1].casefold()
+        if file_extension in piexif_codecs:
             try:
-                im = Image.open(filepath)
-                im = ImageOps.exif_transpose(im)  # Correct image orientation based on EXIF
-                rgb_im = im.convert('RGB')
-                rgb_im.save(filepath)
-
-            except ValueError as e:
-                logging.error(f"Error converting to JPG in {title}: {e}")
+                set_exif(str(filepath), data['geoData']['latitude'], data['geoData']['longitude'],
+                         data['geoData']['altitude'], time_stamp)
+            except Exception as e:
+                print(f"Error setting EXIF data for {filepath}")
+                print(str(e))
+                error_counter += 1
+                continue
+        elif file_extension in video_codecs:
+            try:
+                set_video_metadata(str(filepath), data['geoData']['latitude'], data['geoData']['longitude'], time_stamp)
+            except Exception as e:
+                print(f"Error setting video metadata for {filepath}")
+                print(str(e))
                 error_counter += 1
                 continue
 
-            try:
-                set_exif(str(filepath), data['geoData']['latitude'], data['geoData']['longitude'], data['geoData']['altitude'], time_stamp)
-
-            except Exception as e:  # Error handler
-                logging.error(f"Error setting EXIF data for {filepath}: {e}")
-                error_counter += 1
-                continue
-
-        # creation and modification time
         set_file_times(filepath, time_stamp)
 
-        # MOVE FILE AND DELETE JSON
-        try:
-            os.replace(filepath, matched_output_folder / title)
-            os.remove(output_folder / entry.name)
-        except Exception as e:
-            logging.error(f"Error moving file {filepath}: {e}")
-            error_counter += 1
-            continue
-
+        os.replace(filepath, matched_output_folder / title)
+        os.remove(output_folder / entry.name)
         media_moved.append(title)
         success_counter += 1
 
     success_message = " successes"
     error_message = " errors"
 
-    # UPDATE INTERFACE
     if success_counter == 1:
         success_message = " success"
 
     if error_counter == 1:
         error_message = " error"
 
-    # move any files left to the unmerged folder
     copy_files_only(output_folder, unmatched_output_folder)
 
     window['-PROGRESS_BAR-'].update(100, visible=True)
     window['-PROGRESS_LABEL-'].update(
         "Matching process finished with " + str(success_counter) + success_message + " and " + str(
             error_counter) + error_message + ".", visible=True, text_color='#c0ffb3')
-
